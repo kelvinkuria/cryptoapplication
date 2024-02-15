1722f535fc230bf414d392cbe831394355b01324
mac <mac@macs-MacBook.local>
2024-02-15 23:54:38 +0300

    feat:State for managing the search input value

--- crypto12/src/components/Search.jsx
+++ crypto12/src/components/Search.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import debounce from "lodash.debounce";
+import React, { useState, useContext} from 'react'
+import searchIcon from "../assets/search-icon.svg";
+import { CryptoContext } from "./../context/CryptoContext";
 
-export const Search = () => {
+
+const SearchInput = ({ handleSearch }) => {
+    
+    const [searchText, setSearchText] = useState("");
+    // Accessing context for search data and search functions
+    const { searchData, setCoinSearch, setSearchData } = useContext(CryptoContext);
+  
+    // Event handler for handling input changes in the search field
+    const handleInput = (e) => {
+      const query = e.target.value;
+      setSearchText(query);
+      // Call the handleSearch function passed from the parent component
+      handleSearch(query);
+    };
+  
+   
+  };
+
+
+
+const Search = () => {
   return (
-    <div>Search</div>
+    <div className="relative">
+       <SearchInput handleSearch={debounceFunc} />
+    </div>
+
   )
 }
+
+export default Search
